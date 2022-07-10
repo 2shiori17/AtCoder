192810b433329f947750279cd91412f60dc44a82
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2022-07-10 23:50:19 +0900

    solve(typical90): 001

--- typical90/src/bin/001.rs
+++ typical90/src/bin/001.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize, l: usize,
+        k: usize,
+        mut a: [usize; n],
+    }
+
+    a.push(l);
+
+    let mut ok = 0;
+    let mut ng = l;
+
+    while ng - ok > 1 {
+        let mid = (ok + ng) / 2;
+
+        let mut counter = 0;
+        let mut current = 0;
+        for &val in a.iter() {
+            if val >= current + mid {
+                current = val;
+                counter += 1;
+            }
+        }
+
+        if counter > k {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    println!("{}", ok);
 }
