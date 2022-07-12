f247aa6c1fff4dd359adf73e26e6e206b8eec844
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2022-07-12 20:00:05 +0900

    solve(typical90): 002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
+fn check(x: usize, n: usize) -> Option<String> {
+    let mut cnt = 0;
+    let mut s = vec![];
+    for i in 0..n {
+        if x & (1 << i) != 0 {
+            cnt += 1;
+            s.push('(');
+        } else {
+            cnt -= 1;
+            if cnt < 0 {
+                return None;
+            }
+            s.push(')');
+        }
+    }
+    if cnt == 0 {
+        Some(s.into_iter().collect())
+    } else {
+        None
+    }
+}
+
 fn main() {
-    todo!();
+    input! { n: usize }
+    if n % 2 == 0 {
+        let mut x = vec![];
+        for i in 0..(1 << n) {
+            if let Some(s) = check(i, n) {
+                x.push(s);
+            }
+        }
+        x.sort();
+        println!("{}", x.join("\n"));
+    }
 }
